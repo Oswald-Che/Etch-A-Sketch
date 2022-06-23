e85df3c07567fc7a3b8e18271c4a8be0ffb4fa60
Oswald-Che <cheoswald@yahoo.com>
2022-06-23 22:04:33 +0100

    Add button that changes grid size

--- game.js
+++ game.js
@@ -1,6 +1,21 @@
 const grid = []
 const container = document.querySelector('#container')
-for (let i = 0 ; i < 256 ; i++){
+const button = document.querySelector('#button')
+button.addEventListener("click" , () => {
+    let num = parseInt(prompt("Enter the amount grids \n Max amount = 100"))
+    if (num > 100){
+        alert("ERROR number greater than 100")
+        return
+    }
+    container.innerHTML = ''
+    container.style.cssText = `grid-template-columns:repeat(${num} , 1fr)`
+    let num2 = num ** 2
+    sketch(num2)
+    }
+
+) 
+function sketch(n=625){
+for (let i = 0 ; i < n ; i++){
    grid[i] = document.createElement('div')
    container.appendChild(grid[i])
 }
@@ -8,3 +23,5 @@ for (let i = 0 ; i < 256 ; i++){
  grid.forEach(div => div.addEventListener('mouseenter'  , (e) =>{
     e.target.classList.add('box')   
    }))
+}
+sketch()
