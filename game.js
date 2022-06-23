4b7cf47f1418d420b91d446514b081c5c70478e0
Oswald-Che <cheoswald@yahoo.com>
2022-06-23 20:13:20 +0100

    Fix grid errors and create working grid

--- game.js
+++ game.js
@@ -2,7 +2,9 @@ const grid = []
 const container = document.querySelector('#container')
 for (let i = 0 ; i < 256 ; i++){
    grid[i] = document.createElement('div')
-   grid[i].classList.add('box')
    container.appendChild(grid[i])
-
 }
+
+ grid.forEach(div => div.addEventListener('mouseenter'  , (e) =>{
+    e.target.classList.add('box')   
+   }))
