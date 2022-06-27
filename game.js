b4ff5dacd94ba49d5073177b3463a5fd60b7c6c5
Oswald-Che <cheoswald@yahoo.com>
2022-06-27 17:10:14 +0100

    update random color generator

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+let color = []
 const grid = []
 const container = document.querySelector('#container')
 const button = document.querySelector('#button')
@@ -18,19 +19,36 @@ button.addEventListener("click" , () => {
 function sketch(n=625){
 for (let i = 0 ; i < n ; i++){
    grid[i] = document.createElement('div')
+   grid[i].classList.add(`${i}`)
    container.appendChild(grid[i])
 }
 
  grid.forEach(div => div.addEventListener('mouseenter'  , (e) =>{
-    e.target.classList.add('box')
-    let color  = randColor()
-    e.target.style.cssText = `background-color:${color};`   
-   }, { once : true}) )
+    // e.target.classList.add('box')
+    let num = Number(e.target.classList.value)
+    if (color[num] == undefined){
+        color[num] = randColor()
+    }
+    else{
+        color[num] = shadeColor(color[num] , 10)
+        }
+     console.log(color[num])
+     e.target.style.cssText = `background-color:${color[num]};` 
+   }) )
 }
+//function for Random color generator
 function randColor(){
     let maxVal = 0xFFFFFF
     let randomNumber = Math.floor(Math.random() * maxVal).toString(16)
     let color = randomNumber.padStart(6 , 0)
     return `#${color.toUpperCase()}`
+}
+function shadeColor(color, percent) {
+    var num = parseInt(color.slice(1),16),
+        amt = Math.round(2.55 * percent),
+          R = (num >> 16) - amt,
+          G = (num >> 8 & 0x00FF) - amt,
+          B = (num & 0x0000FF) - amt;
+    return '#' + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1).toUpperCase();
 }
 sketch()
